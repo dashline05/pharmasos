feef79fefe489d4b3fa90b42f3921c4bdd980ca5
dashline05 <contact.pharmasos@gmail.com>
2025-02-15 13:04:41 +0100

    Update pharmacy data structure: - Add multilingual hours format - Update datahi.py script with translations - Add Arabic translations for pharmacy names

--- src/scripts/datahi.py
+++ src/scripts/datahi.py
@@ -75,10 +75,10 @@ LEMATIN_URLS = [
 ]
 
 GUIDE_CITIES = [
-    "/pharmacies-de-garde/rabat.html?date=2025-02-10",
-    "/pharmacies-de-garde/sale.html?date=2025-02-10",
-    "/pharmacies-de-garde/temara.html?date=2025-02-10",
-    "/pharmacies-de-garde/ain-aouda.html?date=2025-02-10"
+    "/pharmacies-de-garde/rabat.html",
+    "/pharmacies-de-garde/sale.html",
+    "/pharmacies-de-garde/temara.html",
+    "/pharmacies-de-garde/ain-aouda.html"
 ]
 
 # Translation dictionaries
