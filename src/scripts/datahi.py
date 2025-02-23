140f7df59fd1a64e157d4bb1a03454c373357c65
dashline05 <contact.pharmasos@gmail.com>
2025-02-23 22:15:38 +0000

    Update datahi.py

--- src/scripts/datahi.py
+++ src/scripts/datahi.py
@@ -76,10 +76,10 @@ LEMATIN_URLS = [
 ]
 
 GUIDE_CITIES = [
-    "/pharmacies-de-garde/rabat.html?date=2025-02-17",
-    "/pharmacies-de-garde/sale.html?date=2025-02-17",
-    "/pharmacies-de-garde/temara.html?date=2025-02-17",
-    "/pharmacies-de-garde/ain-aouda.html?date=2025-02-17"
+    "/pharmacies-de-garde/rabat.html?date=2025-02-24",
+    "/pharmacies-de-garde/sale.html?date=2025-02-24",
+    "/pharmacies-de-garde/temara.html?date=2025-02-24",
+    "/pharmacies-de-garde/ain-aouda.html?date=2025-02-24"
 ]
 
 # Translation dictionaries
